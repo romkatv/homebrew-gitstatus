2204c23560eafb093fc780522a4ca2e7360d80e9
romkatv <roman.perepelitsa@gmail.com>
2020-05-13 11:51:53 +0200

    add gitstatus homebrew tap

---
+++ Formula/gitstatus.rb
@@ -0,0 +1,33 @@
+class Gitstatus < Formula
+  desc "Git status for Bash/Zsh prompt"
+  homepage "https://github.com/romkatv/gitstatus"
+  url "https://github.com/romkatv/gitstatus/archive/v1.1.0.tar.gz"
+  sha256 "f1e6f48cdf39550dd42128a229d9acc681cf92d71130d7920d999295abca8909"
+
+  depends_on "bash" => :test
+  depends_on "zsh"  => :test
+
+  def install
+    system 'sh', '-c', 'GITSTATUS_CACHE_DIR="$PWD"/usrbin ./install -f'
+    prefix.install Dir["*"]
+  end
+
+  def caveats
+    <<~EOS
+      To activate gitstatus in Bash, add the following line to .bashrc:
+
+        source #{opt_prefix}/gitstatus.prompt.sh
+
+      To activate gitstatus in Zsh, add the following line to .zshrc:
+
+        source #{opt_prefix}/gitstatus.prompt.zsh
+    EOS
+  end
+
+  test do
+    assert_match "SUCCESS",
+      shell_output("bash --norc -ic '. #{opt_prefix}/gitstatus.prompt.zsh && gitstatus_start MY && echo SUCCESS'")
+    assert_match "SUCCESS",
+      shell_output("zsh -fic '. #{opt_prefix}/gitstatus.prompt.zsh && gitstatus_start MY && echo SUCCESS'")
+  end
+end
---
+++ README.md
@@ -0,0 +1,27 @@
+# Gitstatus Homebrew Tap
+
+This repository is the official Homebrew Tap for [gitstatus](https://github.com/romkatv/gitstatus).
+
+## Install gitstatus with Homebrew
+
+```zsh
+brew install romkatv/gitstatus/gitstatus
+```
+
+### Use gitstatus from Bash
+
+```bash
+echo 'source /usr/local/opt/gitstatus/gitstatus.prompt.sh' >>~/.bashrc
+```
+
+### Use gitstatus from Zsh
+
+```zsh
+echo 'source /usr/local/opt/gitstatus/gitstatus.prompt.zsh' >>~/.zshrc
+```
+
+## Update gitstatus with Homebrew
+
+```zsh
+brew update && brew upgrade
+```
