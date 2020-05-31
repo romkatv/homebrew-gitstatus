dcec9d6a0dbb6416cfb5499966db025636569567
Roman Perepelitsa <roman.perepelitsa@gmail.com>
2020-05-31 19:19:03 +0200

    upgrade to v1.1.3

--- Formula/gitstatus.rb
+++ Formula/gitstatus.rb
@@ -1,14 +1,14 @@
 class Gitstatus < Formula
   desc "Git status for Bash/Zsh prompt"
   homepage "https://github.com/romkatv/gitstatus"
-  url "https://github.com/romkatv/gitstatus/archive/v1.1.1.tar.gz"
-  sha256 "ae61a5cb2e2bfe822edf4c537880973903ebbfccaa03ec3de9ea64951891ad9d"
+  url "https://github.com/romkatv/gitstatus/archive/v1.1.3.tar.gz"
+  sha256 "10b79ae2ce3d575f7cc3738393357ff2e1bf9ca46133cf998fdb628add26e84b"
 
   depends_on "bash" => :test
   depends_on "zsh"  => [:build, :test]
 
   def install
-    system 'zsh', '-fc', 'GITSTATUS_CACHE_DIR="$PWD"/usrbin ./install -f'
+    system 'zsh', '-fc', 'unset -m "GITSTATUS_*"; GITSTATUS_CACHE_DIR="$PWD"/usrbin ./install -f'
     system 'zsh', '-fc', 'emulate zsh -o no_aliases; for f in *.zsh install; do zcompile -R -- $f.zwc $f || exit; done'
     prefix.install Dir["*"]
   end
