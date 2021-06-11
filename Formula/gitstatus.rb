ff3fe4d1a35bb49b3628a5c98bf60ced201348bd
Roman Perepelitsa <roman.perepelitsa@gmail.com>
2021-06-11 09:22:45 +0200

    upgrade to v1.5.2

--- Formula/gitstatus.rb
+++ Formula/gitstatus.rb
@@ -1,8 +1,8 @@
 class Gitstatus < Formula
   desc "Git status for Bash/Zsh prompt"
   homepage "https://github.com/romkatv/gitstatus"
-  url "https://github.com/romkatv/gitstatus/archive/v1.5.1.tar.gz"
-  sha256 "11f0b7239d04adf75be9377064daa43a35b71192b679d1fd2faea831a7874829"
+  url "https://github.com/romkatv/gitstatus/archive/v1.5.2.tar.gz"
+  sha256 "8eb904b46876c6f8cf5314502f7cb5a25fa1ec220154ce883a1189760da99b98"
 
   depends_on "bash" => :test
   depends_on "zsh"  => :test
