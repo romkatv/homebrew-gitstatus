557c93b59c1359dcbeee3638da6dceed2ba3911d
Roman Perepelitsa <roman.perepelitsa@gmail.com>
2022-01-27 20:58:13 +0100

    remove a dependency on zsh

--- Formula/gitstatus.rb
+++ Formula/gitstatus.rb
@@ -4,9 +4,6 @@ class Gitstatus < Formula
   url "https://github.com/romkatv/gitstatus/archive/v1.5.3.tar.gz"
   sha256 "54db996c8daa2ef6b90ea0490310a7496353c0cbec463c3826d7f61da2a0a5be"
 
-  depends_on "bash" => :test
-  depends_on "zsh"  => :test
-
   def install
     system 'make', 'pkg'
     prefix.install Dir["*"]
@@ -27,8 +24,6 @@ class Gitstatus < Formula
   end
 
   test do
-    assert_match "SUCCESS",
-      shell_output("bash --norc -ic '. #{opt_prefix}/gitstatus.prompt.zsh && gitstatus_start MY && echo SUCCESS'")
     assert_match "SUCCESS",
       shell_output("zsh -fic '. #{opt_prefix}/gitstatus.prompt.zsh && gitstatus_start MY && echo SUCCESS'")
   end
