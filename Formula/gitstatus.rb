4edb605cd22e98ebfb878020c92ad9a229b62f46
Roman Perepelitsa <roman.perepelitsa@gmail.com>
2022-01-28 08:27:22 +0100

    add a dependency on the system zsh

--- Formula/gitstatus.rb
+++ Formula/gitstatus.rb
@@ -4,6 +4,8 @@ class Gitstatus < Formula
   url "https://github.com/romkatv/gitstatus/archive/v1.5.3.tar.gz"
   sha256 "54db996c8daa2ef6b90ea0490310a7496353c0cbec463c3826d7f61da2a0a5be"
 
+  uses_from_macos "zsh" => [:build, :test]
+
   def install
     system 'make', 'pkg'
     prefix.install Dir["*"]
